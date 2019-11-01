ab40063bb475cee05ba625b1dfd80a402bbabef1
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-01 23:13:08 +0000

    Added models

---
+++ CustomerPurchases/Models/CustomerAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerPurchases.Models
+{
+    public class CustomerAddress
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string Address { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
