484697cf270d5dc30fb26cee900dc629166294ce
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-01 23:13:21 +0000

    Created DbContext

---
+++ CustomerPurchases/Data/PurchaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerPurchases.Data
+{
+    public class PurchaseContext : DbContext
+    {
+        public virtual DbSet<Account> Account { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
+        public virtual DbSet<CustomerTel> CustomerTel { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Purchase> Purchase { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=PurchasesDb;");
+        }
+
+    }
+}
