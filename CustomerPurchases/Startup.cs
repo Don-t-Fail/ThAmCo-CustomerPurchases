4a2af43a2a47deddb58799824500561e0dbba48a
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-02 12:01:25 +0000

    Added DbContext to Startup

--- CustomerPurchases/Startup.cs
+++ CustomerPurchases/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerPurchases.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,12 @@ namespace CustomerPurchases
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<PurchaseDbContext>(options =>
+            {
+                var cs = Configuration.GetConnectionString("PurchasesConnection");
+                options.UseSqlServer(cs);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
