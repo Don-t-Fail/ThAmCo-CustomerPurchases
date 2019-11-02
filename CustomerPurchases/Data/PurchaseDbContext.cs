3a5c39b9ca2983072f183ca16264eaa4ba114743
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-02 12:00:34 +0000

    Renamed DbContext

--- CustomerPurchases/Data/PurchaseDbContext.cs
+++ CustomerPurchases/Data/PurchaseDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CustomerPurchases.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 
 namespace CustomerPurchases.Data
 {
-    public class PurchaseContext : DbContext
+    public class PurchaseDbContext : DbContext
     {
         public virtual DbSet<Account> Account { get; set; }
         public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
@@ -15,9 +16,16 @@ namespace CustomerPurchases.Data
         public virtual DbSet<Product> Product { get; set; }
         public virtual DbSet<Purchase> Purchase { get; set; }
 
+        private IHostingEnvironment HostEnv { get; }
+
+        public PurchaseDbContext(DbContextOptions<PurchaseDbContext> options, IHostingEnvironment env) : base(options)
+        {
+            HostEnv = env;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=PurchasesDb;");
+            base.OnConfiguring(optionsBuilder);
         }
 
     }
