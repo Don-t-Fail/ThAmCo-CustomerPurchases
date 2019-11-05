ed3027a81df9f5efc22fd69b865a88e555a2c851
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-05 14:18:42 +0000

    Added recreating the DB in development env

--- CustomerPurchases/Program.cs
+++ CustomerPurchases/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerPurchases.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CustomerPurchases
@@ -14,7 +17,20 @@ namespace CustomerPurchases
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IHostingEnvironment>();
+                var context = services.GetRequiredService<PurchaseDbContext>();
+                if (env.IsDevelopment())
+                {
+                    context.Database.EnsureDeleted();
+                }
+                context.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
