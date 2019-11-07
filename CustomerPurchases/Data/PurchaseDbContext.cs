419c642df2afcb22d51c858c53e46a0cf94d17cc
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-07 16:45:51 +0000

    Added account seed data

--- CustomerPurchases/Data/PurchaseDbContext.cs
+++ CustomerPurchases/Data/PurchaseDbContext.cs
@@ -34,6 +34,23 @@ namespace CustomerPurchases.Data
             if (HostEnv != null && HostEnv.IsDevelopment())
             {
                 //SeedData
+                builder.Entity<Account>().HasData(
+                    new Account { Id = 1, IsStaff = false },
+                    new Account { Id = 2, IsStaff = true },
+                    new Account { Id = 3, IsStaff = false }
+                );
+
+                builder.Entity<CustomerAddress>().HasData(
+                    new CustomerAddress { Id = 1, AccountId = 1, Address = "This is an address"},
+                    new CustomerAddress {  Id = 2, AccountId = 2, Address = "This is an address"},
+                    new CustomerAddress {  Id = 2, AccountId = 1, Address = "This is also an address"}
+                );
+
+                builder.Entity<CustomerTel>().HasData(
+                    new CustomerTel { Id = 1, AccountId = 1, TelNo = "This is a number" },
+                    new CustomerTel { Id = 2, AccountId = 2, TelNo = "This is a number" },
+                    new CustomerTel { Id = 2, AccountId = 1, TelNo = "This is also a number" }
+                );
             }
         }
 
