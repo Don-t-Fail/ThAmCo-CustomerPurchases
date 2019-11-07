8eb04d96f2df59dbace2bc06c6fabdcb58cd43a0
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-07 17:22:16 +0000

    Added addition seed data

--- CustomerPurchases/Data/PurchaseDbContext.cs
+++ CustomerPurchases/Data/PurchaseDbContext.cs
@@ -42,14 +42,26 @@ namespace CustomerPurchases.Data
 
                 builder.Entity<CustomerAddress>().HasData(
                     new CustomerAddress { Id = 1, AccountId = 1, Address = "This is an address"},
-                    new CustomerAddress {  Id = 2, AccountId = 2, Address = "This is an address"},
-                    new CustomerAddress {  Id = 2, AccountId = 1, Address = "This is also an address"}
+                    new CustomerAddress { Id = 2, AccountId = 2, Address = "This is an address"},
+                    new CustomerAddress { Id = 3, AccountId = 1, Address = "This is also an address"}
                 );
 
                 builder.Entity<CustomerTel>().HasData(
                     new CustomerTel { Id = 1, AccountId = 1, TelNo = "This is a number" },
-                    new CustomerTel { Id = 2, AccountId = 2, TelNo = "This is a number" },
-                    new CustomerTel { Id = 2, AccountId = 1, TelNo = "This is also a number" }
+                    new CustomerTel { Id = 2, AccountId = 3, TelNo = "This is a number" },
+                    new CustomerTel { Id = 3, AccountId = 1, TelNo = "This is also a number" }
+                );
+
+                builder.Entity<Product>().HasData(
+                    new Product { Id = 1, Name = "Hydrogen", StockLevel = 1 },
+                    new Product { Id = 2, Name = "Helium", StockLevel = 0 },
+                    new Product { Id = 3, Name = "Lithium", StockLevel = 300 }
+                );
+
+                builder.Entity<Purchase>().HasData(
+                    new Purchase { Id = 1, AccountId = 1, ProductId = 1, AddressId = 1, OrderStatus = "Placed", Qty = 1},
+                    new Purchase { Id = 2, ProductId = 1, AccountId = 1, AddressId = 3, OrderStatus = "Complete", Qty = 4},
+                    new Purchase { Id = 3, AddressId = 2, AccountId = 3, OrderStatus = "In Progress", ProductId = 3, Qty = 2}
                 );
             }
         }
