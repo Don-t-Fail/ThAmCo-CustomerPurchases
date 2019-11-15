f5cb5b99ed89155b929f72dfe197fe08eeaa36ec
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-15 16:48:54 +0000

    Created PurchaseRepository

---
+++ CustomerPurchases/Data/PurchaseRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+
+namespace CustomerPurchases.Data
+{
+    public class PurchaseRepo : IPurchaseRepo
+
+    {
+        public void DeletePurchase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Purchase>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Purchase> GetPurchase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertPurchase(Purchase purchase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePurchase(Purchase purchase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
