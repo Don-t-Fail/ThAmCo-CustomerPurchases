d69db7db13d45ab2d0a0ab304571ff748be6258b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-15 16:47:36 +0000

    Created PurchaseRepo Interface

---
+++ CustomerPurchases/Data/IPurchaseRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+
+namespace CustomerPurchases.Data
+{
+    interface IPurchaseRepo
+    {
+        Task<IEnumerable<Purchase>> GetAll();
+        Task<Purchase> GetPurchase(int id);
+        void InsertPurchase(Purchase purchase);
+        void DeletePurchase(int id);
+        void UpdatePurchase(Purchase purchase);
+        Task Save();
+    }
+}
