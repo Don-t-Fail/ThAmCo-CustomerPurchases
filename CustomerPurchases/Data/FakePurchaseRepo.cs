55fa21cfe15c818f39048c8fdd2b6cce06830d7b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-17 14:51:31 +0000

    Created FakePurchaseRepo

---
+++ CustomerPurchases/Data/FakePurchaseRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+
+namespace CustomerPurchases.Data
+{
+    public class FakePurchaseRepo : IPurchaseRepo
+    {
+
+        private IEnumerable<Purchase> _purchases;
+
+        public FakePurchaseRepo(IEnumerable<Purchase> purchases)
+        {
+            _purchases = purchases;
+        }
+
+        public void DeletePurchase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Purchase>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Purchase> GetPurchase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Purchase>> GetPurchaseByAccount(int accId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertPurchase(Purchase purchase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePurchase(Purchase purchase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
