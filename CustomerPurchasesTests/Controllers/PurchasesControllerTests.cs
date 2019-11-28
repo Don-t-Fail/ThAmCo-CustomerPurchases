ad1f29d6c904ad92db6386e7ff3cda12a55c3ea4
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-28 23:19:51 +0000

    Created Test Project

---
+++ CustomerPurchasesTests/Controllers/PurchasesControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomerPurchases.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerPurchases.Controllers.Tests
+{
+    [TestClass]
+    public class PurchasesControllerTests
+    {
+        [TestMethod]
+        public void GetPurchaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPurchaseTest_NotExists()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PutPurchaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PutPurchaseTest_NotExists()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PostPurchaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PostPurchaseTest_Exists()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PostPurchaseTest_InvalidPurchase()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void DeletePurchaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void DeletePurchaseTest_NotExists()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPurchaseAccountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPurchaseAccountTest_NotExists()
+        {
+            Assert.Fail();
+        }
+    }
+}
