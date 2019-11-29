9cca864b73c3007395bad87a0a2b9a262e4d03cc
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-29 13:31:00 +0000

    Added GetReviewTest

--- CustomerPurchasesTests/Controllers/PurchasesControllerTests.cs
+++ CustomerPurchasesTests/Controllers/PurchasesControllerTests.cs
@@ -2,7 +2,11 @@
 using CustomerPurchases.Controllers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using CustomerPurchases.Data;
+using CustomerPurchases.Models;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace CustomerPurchases.Controllers.Tests
 {
@@ -10,9 +14,23 @@ namespace CustomerPurchases.Controllers.Tests
     public class PurchasesControllerTests
     {
         [TestMethod]
-        public void GetPurchaseTest()
+        public async void GetPurchaseTest()
         {
-            Assert.Fail();
+            // Arrange
+            var purchases = new List<Purchase>
+            {
+                new Purchase { Id = 1, AccountId = 1, AddressId = 1, OrderStatus = "Complete", ProductId = 1, Qty = 2},
+                new Purchase { Id = 2, AccountId = 1, AddressId = 1, OrderStatus = "Complete", ProductId = 2, Qty = 7}
+            };
+            var repo = new FakePurchaseRepo(purchases);
+            var controller = new PurchasesController(repo, new NullLogger<PurchasesController>());
+            var purchaseId = 1;
+
+            // Act
+            var result = await controller.GetPurchase(purchaseId);
+
+            // Assert
+            Assert.AreEqual(purchases.FirstOrDefault(p => p.Id == purchaseId), result.Value);
         }
 
         [TestMethod]
