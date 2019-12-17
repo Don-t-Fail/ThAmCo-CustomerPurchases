9b08f13b5551d263b615575df0355177323ab662
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-17 22:48:10 +0000

    Misc changes

--- CustomerPurchases/Data/Purchases/FakePurchaseRepo.cs
+++ CustomerPurchases/Data/Purchases/FakePurchaseRepo.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using CustomerPurchases.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CustomerPurchases.Models;
 
 namespace CustomerPurchases.Data
 {
