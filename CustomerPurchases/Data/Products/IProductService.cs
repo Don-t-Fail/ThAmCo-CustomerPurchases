d7b1b1f0073320a0974f71a683022b2e6dd571d4
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-17 20:54:32 +0000

    Created ProductService

---
+++ CustomerPurchases/Data/Products/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+using CustomerPurchases.Models.DTOs;
+
+namespace CustomerPurchases.Data.Products
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAll();
+        Task<ProductDto> GetProduct(int id);
+    }
+}
---
+++ CustomerPurchases/Data/Products/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CustomerPurchases.Models;
+using CustomerPurchases.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CustomerPurchases.Data.Products
+{
+    public class ProductService : IProductService
+    {
+        private readonly PurchaseDbContext _context;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _config;
+        private readonly ILogger<ProductService> _logger;
+
+        public ProductService(PurchaseDbContext context, IHttpClientFactory clientFactory, IConfiguration config, ILogger<ProductService> logger)
+        {
+            _context = context;
+            _clientFactory = clientFactory;
+            _config = config;
+            _logger = logger;
+        }
+
+        public async Task<List<ProductDto>> GetAll()
+        {
+            var products = await _context.Product.Select(p => new ProductDto
+            {
+                
+            }).ToListAsync();
+            if (products.Count != 0)
+            {
+                return products;
+            }
+
+            var client = _clientFactory.CreateClient("RetryAndBreak");
+            client.BaseAddress = new Uri(_config["ProductsUrl"]);
+
+            _logger.LogInformation("Contacting Products Service");
+
+            var resp = await client.GetAsync("api/products/GetAll");
+            var productsExt = new List<ProductDto>();
+
+            if (resp.IsSuccessStatusCode)
+            {
+                products = await resp.Content.ReadAsAsync<List<ProductDto>>();
+                return productsExt;
+            }
+
+            return null;
+        }
+
+        public Task<ProductDto> GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
