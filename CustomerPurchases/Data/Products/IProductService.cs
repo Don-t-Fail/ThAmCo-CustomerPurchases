c8a466f25c381df9747f0943c6834a98300c01f7
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-17 22:47:53 +0000

    Created product service

---
+++ CustomerPurchases/Data/Products/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using CustomerPurchases.Models.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerPurchases.Data.Products
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAll();
+        Task<ProductDto> GetProduct(int id);
+    }
+}
