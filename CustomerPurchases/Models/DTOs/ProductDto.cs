5dc962a50f5ef7b3962a0aa6dd1d00aa440b595e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-17 20:52:58 +0000

    Created ProductDTO

---
+++ CustomerPurchases/Models/DTOs/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerPurchases.Models.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
