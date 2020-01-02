94fcbcbbd807452cfd47619120556e868cc4bd26
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-02 16:44:57 +0000

    Created Missing FakeProductService

---
+++ CustomerPurchases/Data/Products/FakeProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPurchases.Models.DTOs;
+
+namespace CustomerPurchases.Data.Products
+{
+    public class FakeProductService : IProductService
+    {
+        private List<ProductDto> _products;
+
+        public FakeProductService(List<ProductDto> products)
+        {
+            _products = products;
+        }
+
+        public async Task<List<ProductDto>> GetAll()
+        {
+            return await Task.FromResult(_products.ToList());
+        }
+
+        public async Task<ProductDto> GetProduct(int id)
+        {
+            return await Task.FromResult(_products.FirstOrDefault(p => p.Id == id));
+        }
+    }
+}
