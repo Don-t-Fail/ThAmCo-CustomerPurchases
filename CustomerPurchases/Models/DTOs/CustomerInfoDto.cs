7208b36b1db667434af5f4dfb5234601ad6bc839
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-02 15:32:58 +0000

    Added DTOs

---
+++ CustomerPurchases/Models/DTOs/CustomerInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerPurchases.Models.DTOs
+{
+    public class CustomerInfoDto
+    {
+        public int CustomerId { get; set; }
+        public List<AddressDto> Addresses { get; set; }
+        public List<TelDto> Tels { get; set; }
+    }
+}
